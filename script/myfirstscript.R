54c382b314bbb69aca39bc8eb874df50fce91808
Francine Salvador <fasalvador@cpp.edu>
2026-07-01 13:34:52 -0700

    Add another plot, colored by species

--- script/myfirstscript.R
+++ script/myfirstscript.R
@@ -1,1 +1,10 @@
 # My First Script in this project
+library(ggplot2)
+library(ratdat)
+
+ggplot(data = complete_old, aes(x = hindfoot_length, y = weight))+
+  geom_point()
+
+# Make a comment
+ggplot(data = complete_old, aes(x = hindfoot_length, y = weight, color = species))+
+  geom_point()
