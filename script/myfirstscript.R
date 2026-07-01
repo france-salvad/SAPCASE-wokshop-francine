1c11887e05ceb8e457cd6116679deff27ec50a6b
Francine Salvador <fasalvador@cpp.edu>
2026-07-01 14:27:54 -0700

    Adds blank line.

--- script/myfirstscript.R
+++ script/myfirstscript.R
@@ -12,3 +12,4 @@ ggplot(data = complete_old, aes(x = hindfoot_length, y = weight, color = species
 # Make a comment
 ggplot(data = complete_old, aes(x = species, y = hindfoot_length, color = species))+
   geom_violin()
+
